25d39087cdd679eb65e9185b59a4c311daad4ca0
송바래(ばれ） <gihun3645@naver.com>
2023-05-30 21:54:29 +0900

    인덱스 페이지 생성

--- src/main/java/com/study/practice/controller/BoardController.java
+++ src/main/java/com/study/practice/controller/BoardController.java
@@ -1,8 +1,15 @@
 package com.study.practice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RequestMapping
+@Controller
 public class BoardController {
-	
+		
+		// 메인 페이지
+		@GetMapping("/")
+		public String index() {
+			// 인덱스 페이지를 리턴
+			return "index";
+		}
 }
