f54269a3172d33e8c71e2ebcd2dd59976d441e19
송바래(ばれ） <gihun3645@naver.com>
2023-06-05 15:32:54 +0900

    페이징 구현2

--- src/main/java/com/study/practice/controller/BoardController.java
+++ src/main/java/com/study/practice/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.study.practice.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -93,10 +94,42 @@ public class BoardController {
 			// 출력할 게시물
 			int displayPost = (num - 1) * postNum;
 			
+			// 한번에 표시할 페이징 번호의 갯수
+			int pageNum_count = 10;
+			
+			// 표시되는 페이지 번호 중 마지막 번호
+			// 마지막 페이지 번호 = ((올림)(현재 페이지 번호 / 한번에 표시할 페이지 번호의 갯수)) * 한번에 표시할 페이지 번호의 갯수
+			int endPageNum = (int)(Math.ceil((double)num / (double)pageNum_count) * pageNum_count);
+			
+			// 표시되는 페이지 번호 중 첫번째 번호
+			// 시작 페이지 = 마지막 페이지 번호 - 한번에 표시할 페이지 번호의 갯수 + 1
+			int startPageNum = endPageNum - (pageNum_count - 1);
+			
+			// 마지막 번호 재계산 
+			int endPageNum_count = (int)(Math.ceil((double)count / (double)pageNum_count));
+			
+			if (endPageNum > endPageNum_count) {
+				endPageNum = endPageNum_count;
+			}
+			
+			boolean prev = startPageNum == 1 ? false : true;
+			boolean next = endPageNum * pageNum_count >= count ? false : true;
+			
+			
 			List<Board> list = null;
 			list = boardDAOImpl.listPage(displayPost, postNum);
 			model.addAttribute("list", list);
 			model.addAttribute("pageNum", pageNum);
-	
+			
+			// 시작 및 끝 번호
+			model.addAttribute("startPageNum", startPageNum);
+			model.addAttribute("endPageNum", endPageNum);
+			
+			// 이전 및 다음
+			model.addAttribute("prev", prev);
+			model.addAttribute("next", next);
+			
+			// 현제 페이지
+			model.addAttribute("select", num);
 		}
 }
