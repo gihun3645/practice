ae790331ef6441a2d0528efaea32b91179fbf2f0
송바래(ばれ） <gihun3645@naver.com>
2023-06-09 15:12:52 +0900

    Enter 키로 버튼 작동하기
    엔터를 부르면 검색 버튼이 눌림

--- src/main/java/com/study/practice/controller/BoardController.java
+++ src/main/java/com/study/practice/controller/BoardController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.mysql.cj.protocol.PacketSentTimeHolder;
 import com.study.practice.domain.Board;
 import com.study.practice.domain.Page;
 import com.study.practice.service.BoardDAOImpl;
